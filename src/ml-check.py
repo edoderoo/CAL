8104b820868885ccd8d6fc9816ad80208d4a315f
Edo de Roo <git@edoderoo.nl>
2023-07-06 22:20:11 +0200

    parse objectnr

--- src/ml-check.py
+++ src/ml-check.py
@@ -1,23 +1,37 @@
 
 type = ''
+cursor = 0
+objectNr = 0
+objectName = ''
+
+def getObjectNr(line):
+   print(line)
 
 def getObjectType(line):
   type='?'
   if line[0:5].upper() == 'TABLE':
      type = 'T'
+     cursor = 6
   if line[0:4].upper() == 'PAGE':
      type = 'P'
+     cursor = 5
   if line[0:8].upper() == 'CODEUNIT':
      type = 'C'
+     cursor = 9
   if line[0:6].upper() == 'REPORT':
      type = 'R'
+     cursor = 7
   if line[0:7].upper() == 'XMLPORT':
      type = 'X'
+     cursor = 8
   if line[0:9].upper() == 'MENUSUITE':
      type = 'X'
-  if type=='?'   : print(line)
+     cursor = 10
+  if type=='?'   : error(line)
+  objectNr = getObjectNr(line[cursor:])
 
 def process_line(line):
+  cursor = 0
   if line[0:6].upper() == 'OBJECT':
      getObjectType(line[7:])
   
