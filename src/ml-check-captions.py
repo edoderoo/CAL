76f76ed3f46df382721c3f075771b96d524a5884
Edo de Roo <git@edoderoo.nl>
2023-07-16 08:47:17 +0200

    fix linux

--- src/ml-check-captions.py
+++ src/ml-check-captions.py
@@ -3,10 +3,17 @@
 from NAVobject import navsrc
 
 
+import platform
+match platform.platform(terse=True)[0:5]:
+  case 'Linux':
+     filename = '/home/edo/Downloads/RMS.txt'
+  case 'Windo': 
+    #filename = r'C:/temp/tmpExample.txt'
+    filename = r'C:/temp/smallCAL.txt'
+  case _:
+    print(platform.platform(terse=True))
+
 
 src = navsrc()
-#filename = r'C:/temp/tmpExample.txt'
-filename = r'C:/temp/smallCAL.txt'
-#filename = '/home/edo/Downloads/RMS.txt'
 src.readsource(filename, 'r')
 src.parse()
